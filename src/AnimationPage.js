74c9514e429b3ef2b74d1d2747fc2b44cf805197
pzy <pzyllll@outlook.com>
2025-05-07 17:37:14 +0800

    1

--- src/AnimationPage.js
+++ src/AnimationPage.js
@@ -1,48 +1,77 @@
 import React from 'react';
 import { Typography, Container, Grid, Card, CardContent, CardMedia } from '@mui/material';
 
+import wudengfenxinniangImage from './assets/wudengfenxinniang.jpg'; 
+import baoshizhigou from './assets/baoshizhigou.jpg'
+import kangna from './assets/kangna.jpg'
+import huiyedaxiaojie from './assets/huiyedaxiaojie.webp'
+import guimiezhiren from './assets/guimiezhiren.webp'
+import jinjidjvren from './assets/jinjidjvren.webp'
+import limulu from './assets/limulu.webp'
+import paiqiushaonian from './assets/paiqiushaonian.webp'
+import daojianshenyv from './assets/daojianshenyv.jpg'
+import huolishaonianwang from './assets/huolishaonianwang.jpg'
+
 // 示例动画数据，实际应用中可以从API获取
 const animations = [
   {
+    title: "火力少年王!!",
+    description: "作品讲述地是：一直视悠悠球为体育运动地悠悠球高手姚杰在东亚悠悠球大赛上落败，由此萌生了放弃悠悠球地念头。在这时，姚杰却遇到了一直将悠悠球当作玩具地凌亮，二人因对悠悠球地观念不同而产生了矛盾和冲突，机缘巧合下，凌亮加入了以姚杰为首地悠悠球队，大家共同训练，经过不断地磨合、相处，姚杰逐渐从凌亮身上体会到玩悠悠球地真正乐趣，放下了过重地得失心，学会享受自己心爱地运动；而凌亮最终也明白到了什么是悠悠球运动，开始认真对待悠悠球。两个对悠悠球概念截然不同地年轻人，最终为着同一个目标，以热诚热血，并肩向着梦想进发",
+    image: huolishaonianwang, 
+    link: "/animation/haikyuu"
+  },
+  {
     title: "咒术回战",
-    description: "《咒术回战》是由芥见下下创作的一部日本漫画作品，讲述了主角虎杖悠仁意外卷入了与诅咒斗争的世界。",
-    image: "https://via.placeholder.com/345x140", // 替换为真实图片链接
+    description: "《咒术回战》是由芥见下下创作的一部日本漫画作品，讲述了主角虎杖悠仁意外卷入了与诅咒斗争的世界！！！！！！！！！！！！！！！！！！！！！！！！ ",
+    image: wudengfenxinniangImage, 
     link: "/animation/jujutsu-kaisen"
   },
   {
-    title: "鬼灭之刃",
-    description: "《鬼灭之刃》是吾峠呼世晴创作的一部奇幻漫画，讲述了一位少年炭治郎为了寻找治愈变成鬼的妹妹的方法而踏上旅程的故事。",
-    image: "https://via.placeholder.com/345x140", // 替换为真实图片链接
+    title: "宝石之国",
+    description: "《宝石之国》讲述了一群以宝石组成的人物，并与月亮上的怪物不断战斗的热血战斗故事，但教导他们的老师似乎并不是那么简单！！！！！！！！！！！！！",
+    image: baoshizhigou, 
     link: "/animation/kimetsu-no-yaiba"
   },
   {
     title: "进击的巨人",
-    description: "《进击的巨人》是一部由谏山创创作的日本漫画，故事围绕着艾连等人对抗神秘的巨人展开，探讨了自由、生存以及人类的真相。",
-    image: "https://via.placeholder.com/345x140",
+    description: "《进击的巨人》是一部由谏山创创作的日本漫画，故事围绕着艾连等人对抗神秘的巨人展开，探讨了自由、生存以及人类的真相！！！！！！！！！！！！！！！！！！",
+    image: jinjidjvren,
+    link: "/animation/attack-on-titan"
+  },
+  {
+    title: "小林家的龙女仆",
+    description: "《小林家的龙女仆》讲述了一个资深社畜程序员小林，在回家的路上与一个误闯入人类世界的龙相遇，后成为小林家的女仆，之后小林又遇见了许多潜入人类世界的不同种类的龙的故事",
+    image: kangna,
     link: "/animation/attack-on-titan"
   },
   {
     title: "我的英雄学院",
-    description: "《我的英雄学院》是由堀越耕平创作的一部超能力题材漫画，描述了一个绝大多数人都拥有“个性”的世界中，没有天生“个性”的少年绿谷出久如何成为英雄的故事。",
-    image: "https://via.placeholder.com/345x140", 
+    description: "《我的英雄学院》是由堀越耕平创作的一部超能力题材漫画，描述了一个绝大多数人都拥有“个性”的世界中，没有天生“个性”的少年绿谷出久如何成为英雄的故事！！！！！！！！！！！！",
+    image: huiyedaxiaojie, 
     link: "/animation/my-hero-academia"
   },
   {
+    title: "鬼灭之刃",
+    description: "《鬼灭之刃》是吾峠呼世晴创作的一部奇幻漫画，讲述了一位少年炭治郎为了寻找治愈变成鬼的妹妹的方法而踏上旅程的故事！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！",
+    image: guimiezhiren, 
+    link: "/animation/kimetsu-no-yaiba"
+  },
+  {
     title: "排球少年!!",
-    description: "《排球少年!!》是古馆春一创作的一部以排球为主题的运动漫画，描绘了日向翔阳在加入乌野高中排球部后，朝着全国大赛的梦想努力奋斗的历程。",
-    image: "https://via.placeholder.com/345x140", 
+    description: "《排球少年!!》是古馆春一创作的一部以排球为主题的运动漫画，描绘了日向翔阳在加入乌野高中排球部后，朝着全国大赛的梦想努力奋斗的历程！！！！！！",
+    image: limulu, 
     link: "/animation/haikyuu"
   },
   {
-    title: "约定的梦幻岛",
-    description: "《约定的梦幻岛》是由白井カイウ原作、出水ぽすか作画的一部科幻悬疑漫画，讲述了孤儿院的孩子们发现他们所谓的家园实际上是一个农场，而他们是被饲养的食物。",
-    image: "https://via.placeholder.com/345x140", 
-    link: "/animation/the-promised-neverland"
+    title: "排球少年!!",
+    description: "《排球少年!!》是古馆春一创作的一部以排球为主题的运动漫画，描绘了日向翔阳在加入乌野高中排球部后，朝着全国大赛的梦想努力奋斗的历程！！！！！！！！！！",
+    image: paiqiushaonian, 
+    link: "/animation/haikyuu"
   },
   {
     title: "刀剑神域",
-    description: "《刀剑神域》是由川原砾创作的一部轻小说改编动画，主要讲述了一场发生在虚拟现实MMORPG游戏中的冒险故事，玩家必须通关游戏才能回到现实世界。",
-    image: "https://via.placeholder.com/345x140", 
+    description: "《刀剑神域》是由川原砾创作的一部轻小说改编动画，主要讲述了一场发生在虚拟现实MMORPG游戏中的冒险故事，玩家必须通关游戏才能回到现实世界！！！！！！！！！！！！！！",
+    image: daojianshenyv, 
     link: "/animation/sword-art-online"
   },
 ];
--- src/NewsPage.js
+++ src/NewsPage.js
@@ -1,12 +1,81 @@
 import React from 'react';
+import { Container, Typography, Grid, Card, CardContent, Box } from '@mui/material';
+import { styled } from '@mui/system';
 
-function NewsPage() {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h2>动态页面 📰</h2>
-      <p>这里是社区动态内容。</p>
-    </div>
-  );
+// 假设这是你的背景图链接或本地路径
+import backgroundImage from './assets/background.jpg'; // 确保替换为实际路径
+
+// 示例社区动态数据
+const communityPosts = [
+    {
+        title: "社区活动：春季徒步",
+        description: "快来加入我们的春季徒步活动，一起探索大自然的美好。",
+        date: "2025-04-15"
+    },
+    {
+        title: "新成员介绍：小李",
+        description: "让我们欢迎新成员小李加入我们的大家庭！",
+        date: "2025-04-10"
+    },
+    // 可以继续添加更多动态...
+];
+
+// 自定义带背景的容器
+const PageContainer = styled(Box)({
+    minHeight: '100vh',
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    padding: '40px 0',
+    position: 'relative',
+});
+
+// 添加半透明黑色遮罩层
+const Overlay = styled(Box)({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    zIndex: 0,
+});
+
+function CommunityPage() {
+    return (
+        <PageContainer>
+            <Overlay />
+            <Container maxWidth="lg" sx={{ position: 'relative', zIndex: 1 }}>
+                <Typography variant="h3" gutterBottom color="white" align="center">
+                    社区动态 🌟
+                </Typography>
+                <Typography variant="h6" paragraph color="lightgray" align="center" mb={4}>
+                    查看最新的社区新闻和活动信息。
+                </Typography>
+
+                {/* 社区动态卡片展示 */}
+                <Grid container spacing={4}>
+                    {communityPosts.map((post, index) => (
+                        <Grid item key={index} xs={12} sm={6} md={4}>
+                            <Card elevation={3} sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                                <CardContent sx={{ flexGrow: 1 }}>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {post.title}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {post.description}
+                                    </Typography>
+                                    <Typography variant="caption" color="text.secondary">
+                                        发布日期: {post.date}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </PageContainer>
+    );
 }
 
-export default NewsPage;
+export default CommunityPage;
